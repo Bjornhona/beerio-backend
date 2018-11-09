1073e3967d2e180909cc277add6ac72c35296a7c
Bjornhona <asaeri3@hotmail.com>
2018-11-09 12:45:30 +0100

    beer/id working

--- routes/beers.js
+++ routes/beers.js
@@ -21,15 +21,17 @@ router.get('/', (req, res, next) => {
     })
 })
 
-router.get('/id', (req, res, next) => {
+router.get('/:id', (req, res, next) => {
+  console.log("hola")
   const id = req.params.id;
   axios.get(`https://api.brewerydb.com/v2/beer/${id}?key=1ff4f5a771c204dd18912e145d2e13ac`)
     .then((result) => {
-      console.log(result);
-      //return res.json(result)
+      console.log(result.data);
+      const data = result.data.data;
+      return res.status(200).json(data)
     })
     .catch((error) => {
-      console.log(error);
+      next(error);
     })
 })
 
