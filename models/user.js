16e0e21c9eed4add96b7ccfc9eb32c06a077453f
Bjornhona <asaeri3@hotmail.com>
2018-11-12 13:46:25 +0100

    data api shows and favorites save in db

--- models/user.js
+++ models/user.js
@@ -4,11 +4,17 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   username: String,
   password: String,
+  favorites: [{
+    id: String,
+    name: String,
+    isOrganic: String,
+    icon: String
+  }]
 }, {
   timestamps: {
     createdAt: 'created_at',
     updatedAt: 'updated_at'
-  },
+  }
 });
 
 const User = mongoose.model('User', userSchema);
